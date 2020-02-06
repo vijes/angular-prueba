79549f15a1306b7f6d0da11a936ae435a97f2760
Victor Jaramillo E <victor87jaramillo@gmail.com>
2020-02-05 22:22:05 -0500

    Actualizacion de código con header y footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { TratamientoComponent } from './componentes/tratamiento/tratamiento.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TratamientoComponent,
+    ContactoComponent,
+    FooterComponent,
+    PortafolioComponent
   ],
   imports: [
     BrowserModule,
